dfe069717d2f722be171dfb0e7c5187b9667cf62
Tomer Abramovich <untradeta@gmail.com>
2022-04-26 12:59:25 +0300

    forgot to expose it

--- src/cephes.rs
+++ src/cephes.rs
@@ -1,11 +1,18 @@
-use libc::{c_double, c_int};
+use libc::{c_double, c_float, c_int};
 
+#[cfg(not(feature = "single"))]
 extern "C" {
     pub fn stdtr(k: c_int, t: c_double) -> c_double;
 }
 
 // used internally by Cephes
+#[cfg(not(feature = "single"))]
 #[no_mangle]
 extern "C" fn isfinite(v: c_double) -> c_int {
     v.is_finite() as _
+}
+
+#[cfg(feature = "single")]
+extern "C" {
+    pub fn stdtrf(k: c_int, t: c_float) -> c_float;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 mod cephes;
 
+#[cfg(not(feature = "single"))]
 #[inline(always)]
 pub fn stdtr(k: i32, t: f64) -> f64 {
     unsafe { cephes::stdtr(k, t) }
+}
+
+#[cfg(feature = "single")]
+#[inline(always)]
+pub fn stdtrf(k: i32, t: f32) -> f32 {
+    unsafe { cephes::stdtrf(k, t) }
 }
